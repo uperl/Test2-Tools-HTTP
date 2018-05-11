81cde90f6d1bde07572b89a13ae1cdf87da4cad3
Graham Ollis <plicease@cpan.org>
2018-05-11 15:06:07 -0400

    move Tx into its own file

--- lib/Test2/Tools/HTTP.pm
+++ lib/Test2/Tools/HTTP.pm
@@ -11,6 +11,7 @@ use Test2::Compare::Wildcard;
 use Test2::Compare::Custom;
 use Test2::Tools::HTTP::UA;
 use Test2::Tools::HTTP::Apps;
+use Test2::Tools::HTTP::Tx;
 use URI;
 use Carp ();
 
@@ -686,47 +687,6 @@ sub psgi_app_del
   my $key = $apps->uri_key($url);
   delete $apps->psgi->{$key};
   return;
-}
-
-package Test2::Tools::HTTP::Tx;
-
-sub req { shift->{req} }
-sub res { shift->{res} }
-sub ok  { shift->{ok}  }
-sub connection_error { shift->{connection_error} }
-
-sub location { shift->{location} }
-
-sub _note_or_diag
-{
-  my($self, $method) = @_;
-  my $ctx = Test2::API::context();
-
-  $ctx->$method($self->req->method . ' ' . $self->req->uri);
-  $ctx->$method($self->req->headers->as_string);
-  $ctx->$method($self->req->decoded_content || $self->req->content);
-  $ctx->$method($self->res->code . ' ' . $self->res->message);
-  $ctx->$method($self->res->headers->as_string);
-  $ctx->$method($self->res->decoded_content || $self->res->content);
-  $ctx->$method("ok = " . $self->ok);
-  
-  $ctx->release;
-}
-
-sub note
-{
-  my($self) = shift;
-  my $ctx = Test2::API::context();
-  $self->_note_or_diag('note');
-  $ctx->release;
-}
-
-sub diag
-{
-  my($self) = shift;
-  my $ctx = Test2::API::context();
-  $self->_note_or_diag('diag');
-  $ctx->release;
 }
 
 package Test2::Tools::HTTP::ResponseCompare;
